96b10f85b8bd03752a0190b9a4a92409a5425884
rohith-git8 <rohith.vuthunuri123@gmail.com>
2024-03-31 11:46:34 +0530

    first commit

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,21 @@
 require("dotenv").config();
 import { Sequelize, DataTypes } from "sequelize";
 
-const POSTGRES_URL = process.env.DATABASE_URL as unknown as string;
-const sequelize = new Sequelize(POSTGRES_URL);
+const POSTGRES_URL = process.env.DATABASE_URL as string;
+
+console.log(POSTGRES_URL);
+
+const sequelize = new Sequelize(POSTGRES_URL, {
+  dialect: "postgres",
+});
 
 async function connectDB() {
-    try {
-        await sequelize.authenticate();
-        console.log("Connection Successful");
-    } catch (error) {
-        console.error("Unable to connect to database: ", error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connection Successful");
+  } catch (error) {
+    console.error("Unable to connect to database: ", error);
+  }
 }
 
 export { connectDB, sequelize, Sequelize, DataTypes };
